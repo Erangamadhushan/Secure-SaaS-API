cb7cfe6662989e274e2c413138770930714d528e
Erangamadhushan <erangamadhushann956@gmail.com>
2025-12-30 23:43:10 +0530

    feat: implement logout and logoutAllSessions functionality with logging

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import rateLimiter from '../middlewares/rateLimit.middleware.js';
 import { protect } from '../middlewares/auth.middleware.js';
-import { login, register } from '../controllers/auth.controller.js';
+import { login, register, logout, logoutAllSessions} from '../controllers/auth.controller.js';
 import { restrictTo } from '../middlewares/role.middleware.js';
 import { refreshAccessToken } from '../modules/auth/auth.controller.js';
 
@@ -29,5 +29,7 @@ router.post("/refresh-token", refreshAccessToken);
 
 router.post("/login", loginValidation, validate, login);
 router.post("/register",registerValidation, validate, register);
+router.post("/logout", protect, logout);
+router.post("/logout-all", protect, logoutAllSessions);
 
 export default router;
